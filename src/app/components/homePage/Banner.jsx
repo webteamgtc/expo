8b914ed7b13d12511961e2e5d7717505da21ed04
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-06-09 18:03:54 +0400

    updated images

--- src/app/components/homePage/Banner.jsx
+++ src/app/components/homePage/Banner.jsx
@@ -27,17 +27,17 @@ const BannerSection = () => {
             </p>
 
             <div className="grid grid-cols-2 md:grid-cols-4 text-center text-sm">
-              {[ 
-                { src: "/icon-05.svg", alt: "Trusted", text: "Trusted & Regulated Worldwide" },
-                { src: "/icon-02.svg", alt: "Traders", text: "Nearly 1 Million Active Traders Worldwide" },
-                { src: "/icon-03.svg", alt: "Deposit", text: "Instant Deposits & Withdrawals" },
-                { src: "/icon-04.svg", alt: "Execution", text: "Secure, Instant & Super-Fast Execution" },
-              ].map(({ src, alt, text }, index) => (
-                <div key={index}>
-                  <Image src={src} alt={alt} width={40} height={40} className="mx-auto mb-2" />
-                  <p className="text-xs text-[#be9b74]">{text}</p>
-                </div>
-              ))}
+              {[
+                    { src: "/homepage/icon-05.svg", alt: "Trusted", text: "Trusted & Regulated Worldwide" },
+                    { src: "/homepage/icon-02.svg", alt: "Traders", text: "Nearly 1 Million Active Traders Worldwide" },
+                    { src: "/homepage/icon-03.svg", alt: "Deposit", text: "Instant Deposits & Withdrawals" },
+                    { src: "/homepage/icon-04.svg", alt: "Execution", text: "Secure, Instant & Super-Fast Execution" },
+                    ].map(({ src, alt, text }, index) => (
+                    <div key={index}>
+                        <Image src={src} alt={alt} width={40} height={40} className="mx-auto mb-2 object-contain" />
+                        <p className="text-xs text-[#be9b74]">{text}</p>
+                    </div>
+                    ))}
             </div>
           </div>
 
