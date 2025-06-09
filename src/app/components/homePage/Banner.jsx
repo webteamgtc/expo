db05612879b6d57f2570e15ee8cff5a1e7100f93
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-06-09 15:28:05 +0400

    updated

--- src/app/components/homePage/Banner.jsx
+++ src/app/components/homePage/Banner.jsx
@@ -35,11 +35,11 @@ const BannerSection = () => {
                 {/* Content Wrapper */}
                 <div className="w-full flex flex-col md:flex-row gap-3 py-10 items-center">
                     {/* Left Content */}
-                    <div className=' md:w-3/5 w-full'>
+                    <div className=' md:w-2/4 w-full'>
 
-                        <h1 className="text-2xl md:text-3xl font-medium mb-4">
-                            Meet Me at <span className="text-primary font-bold">iFX Expo 2025</span> <br />
-                            & I’ll Show You How GTC Can Grow Your Business
+                        <h1 className="text-2xl md:text-4xl font-medium mb-4 leading-tight">
+                            Meet Me at <span className="text-primary font-bold">iFX Expo 2025</span> & <br />
+                            I’ll Show You How GTC Can Grow Your Business
                         </h1>
 
                         <p className="text-gray-600 mb-4">
@@ -50,7 +50,7 @@ const BannerSection = () => {
                             <strong>Save the dates: 17-18-19 June!</strong>
                         </p>
 
-                        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-center text-sm">
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-0 text-center text-sm">
                             <div>
                                 <img src="/homepage/icon-05.svg" alt="Trading" className="mx-auto h-10 mb-2" />
                                 <p className='text-xs text-[#be9b74]'>All-in-One Trading Products & Packages</p>
@@ -72,11 +72,11 @@ const BannerSection = () => {
                     </div>
 
                     {/* Right Image Section */}
-                    <div className="text-center md:w-2/5 w-full">
+                    <div className="text-center md:w-2/4 w-full">
                         <img
                             src="/homepage/02.png"
                             alt="Ahmed Fouad"
-                            className="mx-auto w-60 lg:w-2/4 max-w-sm"
+                            className="mx-auto w-full lg:w-2/4"
                         />
                         <div className="bg-[#263f8f] text-white rounded-tl-xl rounded-br-xl px-6 py-3 w-fit mx-auto text-sm font-medium shadow-md">
                             Ahmed Fouad <br /> <span className="text-xs">Head of Institutional Sales - Dubai</span>
